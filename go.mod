d85eeb934b3796347ad5c25410b70288d30be843
thxrhmn <rahmanmuhaemin@gmail.com>
2024-04-13 15:29:25 +0200

    initial goroutine

--- go.mod
+++ go.mod
@@ -4,6 +4,41 @@ go 1.21.4
 
 toolchain go1.21.5
 
-require github.com/jackpal/bencode-go v1.0.2
+require (
+	github.com/anacrolix/torrent v1.55.0
+	github.com/dustin/go-humanize v1.0.0
+	github.com/google/uuid v1.6.0
+	github.com/jackc/pgx/v4 v4.18.3
+	github.com/qiniu/qmgo v1.1.8
+	go.mongodb.org/mongo-driver v1.11.6
+)
 
-require github.com/dustin/go-humanize v1.0.1
+require (
+	github.com/anacrolix/missinggo v1.3.0 // indirect
+	github.com/anacrolix/missinggo/v2 v2.7.3 // indirect
+	github.com/bradfitz/iter v0.0.0-20191230175014-e8f45d346db8 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/huandu/xstrings v1.3.2 // indirect
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgtype v1.14.0 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/montanaflynn/stats v0.0.0-20171201202039-1bf9dbcd8cbe // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.1 // indirect
+	github.com/xdg-go/stringprep v1.0.3 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/crypto v0.20.0 // indirect
+	golang.org/x/sync v0.3.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,184 +1,260 @@
 package main
 
 import (
-	"encoding/json"
+	"context"
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
+	"sync"
+	"time"
 
+	"github.com/anacrolix/torrent/metainfo"
 	"github.com/dustin/go-humanize"
-	bencode "github.com/jackpal/bencode-go"
+	"github.com/google/uuid"
+	"github.com/qiniu/qmgo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Torrent struct {
-	Name         string         `json:"name"`
-	CreatedBy    string         `json:"created_by"`
-	CreationDate int64          `json:"creation_date"`
-	Announce     string         `json:"announce"`
-	AnnounceList []interface{}  `json:"announce_list"`
-	Size         HumanReadable  `json:"size"`
-	PieceLength  HumanReadable  `json:"piece_length"`
-	TotalFiles   int            `json:"total_files"`
-	Files        []TorrentFiles `json:"files"`
-}
+const (
+	maxGoroutines   = 1
+	torrentPath     = "/root/etc/torrents"
+	torrentDonePath = "/root/etc/torrents-done"
+)
 
 type TorrentFiles struct {
-	Length HumanReadable `json:"length"`
-	Path   []string      `json:"path"`
+	Path   string            `json:"path" bson:"path"`
+	Length HumanReadableSize `json:"length" bson:"length"`
 }
 
-type HumanReadable struct {
-	Size0 int64  `json:"0"`
-	Size1 string `json:"1"`
+type Torrent struct {
+	ID           primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	UUID         string             `json:"uuid" bson:"uuid"`
+	Name         string             `json:"name" bson:"name"`
+	CreatedBy    string             `json:"created_by" bson:"created_by"`
+	CreationDate int64              `json:"creation_date" bson:"creation_date"`
+	TotalFiles   int                `json:"total_files" bson:"total_files"`
+	Hash         string             `json:"hash" bson:"hash"`
+	Magnet       Magnet             `json:"magnet" bson:"magnet"`
+	PieceLength  int64              `json:"piece_length" bson:"piece_length"`
+	Length       HumanReadableSize  `json:"length" bson:"length"`
+	Files        []TorrentFiles     `json:"files" bson:"files"`
+	CreatedAt    int64              `json:"created_at" bson:"created_at"`
+	UpdatedAt    int64              `json:"updated_at" bson:"updated_at"`
+}
+
+type Magnet struct {
+	DisplayName string   `json:"display_name" bson:"display_name"`
+	InfoHash    string   `json:"info_hash" bson:"info_hash"`
+	Trackers    []string `json:"trackers" bson:"trackers"`
+}
+
+type HumanReadableSize struct {
+	Length1 int64  `json:"0" bson:"0"`
+	Length2 string `json:"1" bson:"1"`
 }
 
 func main() {
-	var filePath string
+	// Waktu sebelum eksekusi operasi database
+	start := time.Now()
 
-	flag.StringVar(&filePath, "file", "", "Path to the torrent file")
+	var dirPath string
+
+	flag.StringVar(&dirPath, "dir", "", "Path to the directory containing .torrent files")
 	flag.Parse()
 
-	file, err := os.Open(filePath)
+	if dirPath == "" {
+		log.Fatal("Please provide a directory path using the -dir flag")
+	}
+
+	// MongoDB connection URI
+	uri := "mongodb://thxrhmn:thxrhmn@95.111.231.236:27017/"
+	// uri := "mongodb://thxrhmn:thxrhmn@localhost:27017/"
+	dbName := "torsnif"
+	collectionName := "torrents"
+
+	// Create a MongoDB session
+	ctx := context.Background()
+	client, err := qmgo.NewClient(ctx, &qmgo.Config{Uri: uri})
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		log.Fatal(err)
 	}
-	defer file.Close()
+	defer client.Close(ctx)
 
-	decoder, err := bencode.Decode(file)
+	// Collection reference
+	collection := client.Database(dbName).Collection(collectionName)
+
+	// Find all .torrent files in the directory
+	torrentFiles, err := findTorrentFiles(dirPath)
 	if err != nil {
-		fmt.Println("Error decoding file:", err)
-		return
+		log.Fatal(err)
 	}
 
-	decoderMap, ok := decoder.(map[string]interface{})
-	if !ok {
-		fmt.Println("Error: decoder is not a map[string]interface{} type")
-		return
-	}
+	// Create a wait group for Goroutines
+	var wg sync.WaitGroup
 
-	var torrents []TorrentFiles
+	// Buat buffered channel untuk mengontrol jumlah goroutine
+	semaphore := make(chan struct{}, maxGoroutines)
 
-	info, ok := decoderMap["info"].(map[string]interface{})
-	if !ok {
-		fmt.Println("Error: info is not a map[string]interface{} type")
-		return
-	}
+	for _, filePath := range torrentFiles {
+		wg.Add(1)
+		semaphore <- struct{}{} // Acquire semaphore
+		go func(filePath string) {
+			defer func() {
+				wg.Done()
+				<-semaphore // Release semaphore
+			}()
 
-	name := info["name"].(string)
-	pieceLength := info["piece length"].(int64)
-	pieceLengthString := humanize.Bytes(uint64(pieceLength))
-	createdBy := decoderMap["created by"].(string)
-	creationDate := decoderMap["creation date"].(int64)
-	var announce string
-	announceInterface := decoderMap["announce"]
-	if announceInterface != nil {
-		announce = decoderMap["announce"].(string)
-	}
-	announceList := extractAnnounceList(decoderMap["announce-list"])
-	var torrentSize int64
-
-	files, ok := info["files"].([]interface{})
-	if ok {
-		size, tor := extractFiles(files)
-		torrents = tor
-		torrentSize = size
-	}
-
-	data := Torrent{
-		Name:         name,
-		CreatedBy:    createdBy,
-		CreationDate: creationDate,
-		Announce:     announce,
-		AnnounceList: announceList,
-		Size: HumanReadable{
-			Size0: torrentSize,
-			Size1: humanize.Bytes(uint64(torrentSize)),
-		},
-		PieceLength: HumanReadable{
-			Size0: pieceLength,
-			Size1: pieceLengthString,
-		},
-		TotalFiles: len(torrents),
-		Files:      torrents,
-	}
-
-	jsonData, err := json.MarshalIndent(data, "", "    ")
-	if err != nil {
-		fmt.Println("Error marshalling to JSON:", err)
-		return
-	}
-
-	// Write the JSON data to the file
-	jsonPath := filePath + ".json"
-	err = os.WriteFile(jsonPath, jsonData, 0644)
-	if err != nil {
-		fmt.Println("Error writing to file:", err)
-		return
-	}
-
-	fmt.Println("JSON data successfully written to", jsonPath)
-}
-
-func extractAnnounceList(announceList interface{}) []interface{} {
-	var result []interface{}
-
-	list, ok := announceList.([]interface{})
-	if !ok {
-		return result
-	}
-
-	for _, val := range list {
-		innerSlice, ok := val.([]interface{})
-		if !ok || len(innerSlice) == 0 {
-			continue
-		}
-
-		result = append(result, innerSlice[0])
-	}
-
-	return result
-}
-
-func extractFiles(files []interface{}) (int64, []TorrentFiles) {
-	var result []TorrentFiles
-
-	var torrentSize int64
-
-	for _, file := range files {
-		fileMap, ok := file.(map[string]interface{})
-		if !ok {
-			continue
-		}
-
-		length, ok := fileMap["length"].(int64)
-		if !ok {
-			continue
-		}
-
-		pathArray, ok := fileMap["path"].([]interface{})
-		if !ok {
-			continue
-		}
-
-		// Convert the array elements to strings
-		var pathStrings []string
-		for _, elem := range pathArray {
-			if str, ok := elem.(string); ok {
-				pathStrings = append(pathStrings, str)
+			err := processTorrentFile(ctx, collection, filePath)
+			if err != nil {
+				log.Printf("Error processing %s: %v\n", filePath, err)
 			}
+		}(filePath)
+	}
+
+	// Wait for all Goroutines to finish
+	wg.Wait()
+
+	fmt.Println("All torrents processed successfully")
+
+	// Waktu setelah eksekusi operasi database
+	end := time.Now()
+	// Menghitung selisih waktu
+	duration := end.Sub(start)
+	fmt.Println("Waktu eksekusi:", duration)
+}
+
+func findTorrentFiles(dirPath string) ([]string, error) {
+	var torrentFiles []string
+
+	// Walk through the directory and find .torrent files
+	err := filepath.Walk(dirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
 		}
+		if !info.IsDir() && filepath.Ext(path) == ".torrent" {
+			torrentFiles = append(torrentFiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return torrentFiles, nil
+}
+
+func processTorrentFile(ctx context.Context, collection *qmgo.Collection, torrentPath string) error {
+	torrentInfo, err := metainfo.LoadFromFile(torrentPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	infoHash := torrentInfo.HashInfoBytes()
+	info, err := torrentInfo.UnmarshalInfo()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	magnet := torrentInfo.Magnet(&infoHash, &info)
+
+	var files []TorrentFiles
+	for _, file := range info.Files {
+		path := file.DisplayPath(&info)
+		length := file.Length
 
 		data := TorrentFiles{
-			Length: HumanReadable{
-				Size0: length,
-				Size1: humanize.Bytes(uint64(length)),
+			Path: path,
+			Length: HumanReadableSize{
+				Length1: length,
+				Length2: humanize.Bytes(uint64(length)),
 			},
-			Path: pathStrings,
 		}
-		torrentSize += length
-		result = append(result, data)
+		files = append(files, data)
 	}
 
-	return torrentSize, result
+	// Generate a UUID
+	uuid := uuid.New().String()
+
+	torrent := Torrent{
+		ID:           primitive.NewObjectID(),
+		UUID:         uuid,
+		Name:         magnet.DisplayName,
+		CreatedBy:    torrentInfo.CreatedBy,
+		CreationDate: torrentInfo.CreationDate,
+		PieceLength:  info.PieceLength,
+		Length: HumanReadableSize{
+			Length1: info.TotalLength(),
+			Length2: humanize.Bytes(uint64(info.TotalLength())),
+		},
+		TotalFiles: len(info.Files),
+		Hash:       infoHash.HexString(),
+		Magnet: Magnet{
+			DisplayName: magnet.DisplayName,
+			InfoHash:    magnet.InfoHash.HexString(),
+			Trackers:    magnet.Trackers,
+		},
+		Files:     files,
+		CreatedAt: time.Now().Unix(), // Set createdAt field with current Unix time
+		UpdatedAt: time.Now().Unix(), // Set updatedAt field with current Unix time
+	}
+
+	// Lakukan query ke database untuk mencari hash yang sudah ada
+	existingTorrent := &Torrent{}
+	err = collection.Find(ctx, bson.M{"hash": torrent.Hash}).One(existingTorrent)
+	if err != nil {
+		if err != qmgo.ErrNoSuchDocuments {
+			return fmt.Errorf("error checking existing torrent: %w", err)
+		}
+	} else {
+		fmt.Printf("❌  %s already in database\n", torrent.Magnet.InfoHash)
+		// fmt.Printf("❌  %s Torrent dengan hash yang sama sudah ada dalam database\n", torrent.Magnet.InfoHash)
+		// Move the .torrent file
+		err = deleteTorrentFile(torrentPath)
+		if err != nil {
+			return fmt.Errorf("error delete torrent file: %w", err)
+		}
+		return nil
+	}
+
+	// Insert the data into the collection
+	_, err = collection.InsertOne(ctx, &torrent)
+	if err != nil {
+		return fmt.Errorf("❌ error inserting into collection: %w", err)
+	}
+
+	fmt.Printf("✅  %s\n", torrent.Hash)
+	// fmt.Printf("✅  %s\n", torrent.Magnet.DisplayName)
+
+	// Move the .torrent file
+	err = moveTorrentFile(torrentPath)
+	if err != nil {
+		return fmt.Errorf("error moving torrent file: %w", err)
+	}
+
+	return nil
+}
+
+func moveTorrentFile(torrentPath string) error {
+	// Define the destination directory
+	destDir := torrentDonePath
+	// Extract the filename
+	_, fileName := filepath.Split(torrentPath)
+	destPath := filepath.Join(destDir, fileName)
+	// Move the file
+	err := os.Rename(torrentPath, destPath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func deleteTorrentFile(filePath string) error {
+    err := os.Remove(filePath)
+    if err != nil {
+        return fmt.Errorf("error deleting file: %w", err)
+    }
+    return nil
 }
